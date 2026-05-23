0c70d019f9ac9eeab34955437f49d36ea278fb9e
brmatthy <109791839+brmatthy@users.noreply.github.com>
2026-05-23 11:36:06 +0200

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,25 @@
 {
-  description = "A very basic flake";
+  description = "Noctis development shell";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        python314
+        python314Packages.pip
+      ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+      shellHook = ''
+        echo -n "Noctis development shell activated: "
+        python --version
+      '';
+    };
   };
 }
